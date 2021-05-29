e589e1036d6b96b76763a1376f74675e97949ee4
henryelliottli <li.henry.elliott@gmail.com>
2021-05-30 00:19:17 +0800

    attempt 2 for deployment. Changed db.js

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ require("dotenv").config(); //using ENV variables in .env
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; //heroku addons
+const prodConfig = {
+    connectionString: process.env.DATABASE_URL, //heroku addon
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
 
 const pool = new Pool({
   connectionString:
